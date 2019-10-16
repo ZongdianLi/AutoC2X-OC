8f72430c7ee3a0a162b45682a6687703aabc3f58
大井貴晴 <ooitakaharu@ooitakaharunoMacBook-Air.local>
2019-10-16 16:27:27 +0900

    fix

--- pingApp/src/pingApp.cpp
+++ pingApp/src/pingApp.cpp
@@ -1,7 +1,7 @@
 #define ELPP_THREAD_SAFE
 #define ELPP_NO_DEFAULT_LOG_FILE
 
-#include "caservice.h"
+#include "pingApp.h"
 #include <google/protobuf/text_format.h>
 #include <unistd.h>
 #include <iostream>
