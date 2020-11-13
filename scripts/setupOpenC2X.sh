eeba339cd8e7c01718928168103c349d82f01185
mizu <masayam1214@me.com>
2020-11-13 14:59:06 +0900

    fix

--- scripts/setupOpenC2X.sh
+++ scripts/setupOpenC2X.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 apt-get install libzmq3-dev libboost-all-dev protobuf-compiler libprotobuf-dev libgps-dev gpsd gpsd-clients libnl-3-200 libnl-3-dev libnl-genl-3-200 libnl-genl-3-dev sqlite3 libsqlite3-dev tmux asn1c build-essential cmake doxygen libnl-route-3-dev
 echo "deb [trusted=yes] https://dl.bintray.com/fynnh/debian xenial main" | sudo tee -a /etc/apt/sources.list
 apt-get update
